2f64eb6a7f261b96b7742c594ad03f0987c84398
Anbu Selvam <anbuselvamv27@gmail.com>
2025-12-30 11:37:19 +0000

    updated

--- sync_counter/mod_n_counter/mod_n_counter.v
+++ sync_counter/mod_n_counter/mod_n_counter.v
@@ -1,2 +1,13 @@
-module mod_n_count;
+module mod_n_count #(parameter n = 10,width = 4)(input clk,rst,
+	output reg[width-1:0]count);
+
+always@(posedge clk)begin
+	if(rst)
+		count <= 0;
+	else if(count == n - 1)
+		count <= 0;
+	else
+		count <= count + 1;
+end
+
 endmodule
--- sync_counter/mod_n_counter/mod_n_counter_tb.v
+++ sync_counter/mod_n_counter/mod_n_counter_tb.v
@@ -1,2 +1,24 @@
 module mod_n_count_tb;
+reg clk,rst;
+wire [3:0] count;
+
+mod_n_count #(.n(10),.width(4)) mod_n_count_dut(.clk(clk),.rst(rst),.count(count));
+
+always #5 clk = ~clk;
+
+initial begin
+	clk = 0;
+	rst = 1;
+	
+	$monitor("time = %0t|rst = %b|count = %b|",$time,rst,count);
+
+	#10;
+	rst = 0;
+	#110;
+	$finish;
+end
+initial begin 
+	$dumpfile("mod_n_counter.vcd");
+	$dumpvars(0,mod_n_count_tb);
+end
 endmodule
