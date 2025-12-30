198f52fa11d09d9a11bb98651022f1daa7fb9a73
Anbu Selvam <anbuselvamv27@gmail.com>
2025-12-30 06:02:11 +0000

    updated

--- sync_counter/up_counter/up_counter_tb.v
+++ sync_counter/up_counter/up_counter_tb.v
@@ -1,2 +1,19 @@
 module up_count_tb;
+reg clk,rst;
+wire [3:0]count;
+up_count up_dut(.clk(clk),.rst(rst),.count(count));
+always #5 clk = ~clk;
+initial begin 
+	clk = 0;
+	rst = 1;
+	$monitor("time = %0t|rst = %b|count = %b",clk,rst,count);
+	#10;
+	rst = 0;
+	#200;
+	$finish;
+end
+initial begin
+	$dumpfile("up_counter.vcd");
+	$dumpvars(0,up_count_tb);
+end
 endmodule;
