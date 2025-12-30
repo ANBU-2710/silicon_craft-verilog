44009078c6c093eb5f628d7b88dfa0cbf42b843f
Anbu Selvam <anbuselvamv27@gmail.com>
2025-12-30 09:11:42 +0000

    down_count updated

--- sync_counter/down_counter/down_counter.v
+++ sync_counter/down_counter/down_counter.v
@@ -1,2 +1,10 @@
-module down_count;
+module down_count(input clk,rst,output reg[3:0] count);
+
+always@(posedge clk)begin
+	if(rst)
+		count <= 4'b1111;
+	else
+		count <= count - 1;
+end
+
 endmodule
--- sync_counter/down_counter/down_counter_tb.v
+++ sync_counter/down_counter/down_counter_tb.v
@@ -1,2 +1,23 @@
 module down_count_tb;
+reg clk,rst;
+wire [3:0]count;
+
+down_count d_count_dut(.clk(clk),.rst(rst),.count(count));
+
+always #5 clk = ~clk;
+
+initial begin 
+	clk = 0;
+	rst = 1;
+	
+	$monitor("time = %0t | rst = %b | count = %b",$time,rst,count);
+	#10;
+	rst = 0;
+	#200;
+	$finish;
+end
+initial begin
+	$dumpfile("down_counter.vcd");
+	$dumpvars(0,down_count_tb);
+end
 endmodule
