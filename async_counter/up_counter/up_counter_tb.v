034c7be35876381f2e30d04a7f6c1fbc1f2bd93d
Anbu Selvam <anbuselvamv27@gmail.com>
2026-01-03 05:22:47 +0000

    update changes

--- async_counter/up_counter/up_counter_tb.v
+++ async_counter/up_counter/up_counter_tb.v
@@ -1,2 +1,19 @@
 module up_count_tb;
+reg clk,rst;
+wire [3:0] count;
+up_count up_dut(.clk(clk),.rst(rst),.q(count));
+always #5 clk = ~clk;
+initial begin
+	clk = 0;
+	rst = 1;
+	$monitor("time = %0t|clk = %b|rst = %b|count = %b|",$time,clk,rst,count);
+	#10 rst = 0;
+	#155;
+	$finish;
+end
+initial begin
+	$dumpfile("up_count.vcd");
+	$dumpvars(0,up_count_tb);
+end
+
 endmodule
